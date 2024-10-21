cb8bf8efd7f330aea524c9c3770c2c3eb6b716d8
ryosuke.ochiai <ryosuke.ochiai@petabit.co.jp>
2024-10-21 12:47:58 +0900

    GitHub Pages デプロイ設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ryo-ochi.github.io',
+  base: '/astro-tutorial',
+});
