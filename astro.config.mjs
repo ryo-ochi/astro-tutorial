d5c0afc27c721af7c8bfa1ca6b6aa22179629c2d
ryosuke.ochiai <ryosuke.ochiai@petabit.co.jp>
2024-10-22 12:56:59 +0900

    Preact 追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ryo-ochi.github.io',
   base: '/astro-tutorial',
+  integrations: [preact()],
 });
