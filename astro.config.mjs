c564fb1279ebaae94af8d0bb008d89af58c99c0d
ryosuke.ochiai <ryosuke.ochiai@petabit.co.jp>
2024-11-20 11:25:45 +0900

    React インストール

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ryo-ochi.github.io',
   base: '/astro-tutorial',
-  integrations: [preact()],
+  integrations: [preact(), react()],
 });
